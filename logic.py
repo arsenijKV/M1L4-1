0a61ccd3b1f57b34cda1a9f7fa892f4a36364fbf
arsenijKV <arsenij.kononoff@gmail.com>
2024-11-24 18:54:47 +0200

    add class, new main.py

--- logic.py
+++ logic.py
@@ -11,7 +11,9 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
-
+        self.hp = randint(10,100)
+        self.power = randint(10,100)
+        self.abilities = self.get_abilities
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
@@ -36,14 +38,42 @@ class Pokemon:
         else:
             return "Pikachu"
 
-
-    # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"""Имя твоего покеомона: {self.name}
+        Cила покемона: {self.power}
+        Здоровье покемона: {self.hp}"""
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"""Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}
+            Здоровье @{enemy.pokemon_trainer} теперь {enemy.hp}"""
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
+class Wizard(Pokemon):
+
+    def info(self): # доп. задание
+        return "У тебя покемон-волшебник \n\n" + super().info()
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+
+    def info(self): # доп. задание
+        return "У тебя покемон-боец \n\n" + super().info()
 
 
--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import telebot
 from config import token
 
 from logic import Pokemon
-
+from logic import Wizard
+from logic import Fighter
+from random import randint
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -19,6 +21,19 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+
+@bot.message_handler(commands=['attack'])
+def attack_pok(message):
+    if message.reply_to_message:
+        if message.reply_to_message.from_user.username in Pokemon.pokemons.keys() and message.from_user.username in Pokemon.pokemons.keys():
+            enemy = Pokemon.pokemons[message.reply_to_message.from_user.username]
+            pok = Pokemon.pokemons[message.from_user.username]
+            res = pok.attack(enemy)
+            bot.send_message(message.chat.id, res)
+        else:
+            bot.send_message(message.chat.id, "Сражаться можно только с покемонами")
+    else:
+            bot.send_message(message.chat.id, "Чтобы атаковать, нужно ответить на сообщения того, кого хочешь атаковать")
 
 bot.infinity_polling(none_stop=True)
 
